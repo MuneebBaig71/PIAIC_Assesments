d6e4f16b023ec3210bf3b365c21b216df0da9d54
Muneeb <muneebbaig71@gmail.com>
2024-11-10 11:53:37 +0500

    updated

---
+++ List/get_first_element.py
@@ -0,0 +1,20 @@
+def get_first_element(lst):
+    """
+    Prints the first element of a provided list.
+    """
+
+    print(f"first element of the list is '{lst[0]}'")
+def get_lst():
+    """
+    Prompts the user to enter one element of the list at a time and returns the resulting list.
+    """
+    lst = []
+    elem: str = input("Please enter an element of the list or press enter to stop. ")
+    while elem != "":
+        lst.append(elem)
+        elem = input("Please enter an element of the list or press enter to stop. ")
+    return lst
+
+
+lst = get_lst()
+get_first_element(lst)
